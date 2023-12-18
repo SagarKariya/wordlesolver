fee34edbfe416f0c15821b92c7bcf4da95de52f3
SagarKariya <60439654+SagarKariya@users.noreply.github.com>
2023-12-19 01:19:46 +0530

    Create locator.py

---
+++ locator.py
@@ -0,0 +1,37 @@
+
+locator = {
+
+    'close_sign' : '//*[@data-testid="icon-close"]',
+
+    'q' : '//*[@id="wordle-app-game"]/div[2]/div[1]/button[1]',
+    'w' : '//*[@id="wordle-app-game"]/div[2]/div[1]/button[2]',
+    'e' : '//*[@id="wordle-app-game"]/div[2]/div[1]/button[3]',
+    'r' : '//*[@id="wordle-app-game"]/div[2]/div[1]/button[4]',
+    't' : '//*[@id="wordle-app-game"]/div[2]/div[1]/button[5]',
+    'y' : '//*[@id="wordle-app-game"]/div[2]/div[1]/button[6]',
+    'u' : '//*[@id="wordle-app-game"]/div[2]/div[1]/button[7]',
+    'i' : '//*[@id="wordle-app-game"]/div[2]/div[1]/button[8]',
+    'o' : '//*[@id="wordle-app-game"]/div[2]/div[1]/button[9]',
+    'p' : '//*[@id="wordle-app-game"]/div[2]/div[1]/button[10]',
+
+    'a' : '//*[@id="wordle-app-game"]/div[2]/div[2]/button[1]',
+    's' : '//*[@id="wordle-app-game"]/div[2]/div[2]/button[2]',
+    'd' : '//*[@id="wordle-app-game"]/div[2]/div[2]/button[3]',
+    'f' : '//*[@id="wordle-app-game"]/div[2]/div[2]/button[4]',
+    'g' : '//*[@id="wordle-app-game"]/div[2]/div[2]/button[5]',
+    'h' : '//*[@id="wordle-app-game"]/div[2]/div[2]/button[6]',
+    'j' : '//*[@id="wordle-app-game"]/div[2]/div[2]/button[7]',
+    'k' : '//*[@id="wordle-app-game"]/div[2]/div[2]/button[8]',
+    'l' : '//*[@id="wordle-app-game"]/div[2]/div[2]/button[9]',
+
+    'z' : '//*[@id="wordle-app-game"]/div[2]/div[3]/button[2]',
+    'x' : '//*[@id="wordle-app-game"]/div[2]/div[3]/button[3]',
+    'c' : '//*[@id="wordle-app-game"]/div[2]/div[3]/button[4]',
+    'v' : '//*[@id="wordle-app-game"]/div[2]/div[3]/button[5]',
+    'b' : '//*[@id="wordle-app-game"]/div[2]/div[3]/button[6]',
+    'n' : '//*[@id="wordle-app-game"]/div[2]/div[3]/button[7]',
+    'm' : '//*[@id="wordle-app-game"]/div[2]/div[3]/button[8]',
+
+    'ENTER': '//*[@id="wordle-app-game"]/div[2]/div[3]/button[1]',
+    'TILES': '//*[@data-testid="tile"]'
+}
